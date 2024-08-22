c7979aa0cc68a011a57c563b89f451253e200389
halla <halla@imsweb.com>
2024-08-22 09:59:45 -0400

    Set the ComVisible flag for the TNMStagingCSharp DLL to true.

--- TNMStagingCSharp/TNMStagingCSharp/Properties/AssemblyInfo.cs
+++ TNMStagingCSharp/TNMStagingCSharp/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("15a927c7-9920-4fad-9dae-b1088a7021a3")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.8.0.0")]
-[assembly: AssemblyFileVersion("4.8.0.0")]
+[assembly: AssemblyVersion("4.8.1.0")]
+[assembly: AssemblyFileVersion("4.8.1.0")]
